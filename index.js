976abba88f5b8fa15fdc1a88598ab2029ba4228c
mitsoshxc <mitsoshxc@gmail.com>
2017-08-09 18:57:39 +0300

    Server - Client connection

--- index.js
+++ index.js
@@ -2,24 +2,30 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = [];
 // app.get('/', function(req, res){
 //   res.sendFile(__dirname + '/index.html');
 // });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(1969, function(){
+  console.log('listening on *:1969');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  // console.log('a user connected with id: ' + socket.id);
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    console.log('User  disconnected');
   });
   socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
-    });
-    socket.on('new user', function(user){
-      // io.emit('new user', 'User ' + user + ' connected');
-      console.log('User connected');
-    });
+    io.emit('chat message', msg);
+  });
+  socket.on('join', function(data){
+    users.push(data.user);
+    // user = {id: socket.id, name: data.user};
+    socket.join('global');
+    socket.join(data.user);
+    io.emit('new-user', users);
+    console.log('User ' + data.user + ' connected with id ' + socket.id);
+    //console.log(io.sockets.adapter.rooms);
+  });
 });
