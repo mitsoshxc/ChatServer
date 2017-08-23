dd7c3da122aa422b7bfcab2f2ac8b77628f9547c
mitsoshxc <mitsoshxc@gmail.com>
2017-08-23 16:35:26 +0300

    groups start

--- index.js
+++ index.js
@@ -7,35 +7,39 @@ var users = [];
 //   res.sendFile(__dirname + '/index.html');
 // });
 
-http.listen(1969, function(){
+http.listen(1969, function() {
   console.log('listening on *:1969');
 });
 
-io.on('connection', function(socket){
-  socket.on('disconnect', function(){
+io.on('connection', function(socket) {
+  socket.on('disconnect', function() {
     console.log('User  ' + users[users.findIndex(p => p.id == socket.id)].name + '  disconnected');
     io.emit('user-left', users[users.findIndex(p => p.id == socket.id)].name);
     users.splice(users.findIndex(p => p.id == socket.id), 1);
     io.emit('new-user', users);
   });
 
-  socket.on('chat-message', function(msg){
+  socket.on('chat-message', function(msg) {
     console.log(msg);
     io.emit('new-message', {
-                            message: msg,
-                            user: users[users.findIndex(p => p.id == socket.id)].name
-                           });
+      message: msg,
+      user: users[users.findIndex(p => p.id == socket.id)].name
+    });
   });
 
-  socket.on('join', function(data){
-      users.push({
-                  id: socket.id,
-                  name: data.user
-                });
-    socket.join('global');
+  socket.on('join', function(data) {
+    users.push({
+      id: socket.id,
+      name: data.user
+    });
+    socket.join('First Group');
     socket.join(data.user);
     io.emit('new-user', users);
     console.log('User ' + data.user + ' connected with id ' + socket.id);
+  });
+
+  socket.on('group-change', function(group) {
+    socket.join(group);
   });
 
 });
